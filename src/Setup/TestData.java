048a90cb86b448e7d76e64b166e723522465830a
yuvanshankar <yuvanshankarraja27@gmail.com>
2020-06-15 18:48:52 +0530

    commit

--- src/Setup/TestData.java
+++ src/Setup/TestData.java
@@ -2,12 +2,15 @@ package Setup;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class TestData {
-	int dataNo = 1;
-
-	// **** Method to specify the Data needed from Excel ****
+	public static Properties property;
+	int dataNo;
+	// ** Method to specify the Data needed from Excel **
 	public String get_Data(String SheetName, String Value) throws IOException {
+		property = BaseClass.invoke_Property_File();
+		 dataNo = Integer.parseInt(property.getProperty("UserDataNo"));
 		ImportFromExcel readData = new ImportFromExcel();
 		ArrayList<String> list = readData.getdata(SheetName, Value);
 		String data = (String) list.get(dataNo);
